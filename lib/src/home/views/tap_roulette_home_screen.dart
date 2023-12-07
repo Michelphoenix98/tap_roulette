af2e170a5e5e5627152a7168a7a45e8f6febdd3c
Michel Thomas <michelphoenix98@gmail.com>
2023-12-07 21:24:44 +0530

    fix: Remove commented code

--- lib/src/home/views/tap_roulette_home_screen.dart
+++ lib/src/home/views/tap_roulette_home_screen.dart
@@ -46,10 +46,6 @@ class _TapRouletteHomeScreenState extends State<TapRouletteHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      //   title: Text(widget.title),
-      // ),
       appBar: const CustomAppBar(),
       body: MultiBlocProvider(
         providers: [
@@ -102,10 +98,6 @@ class _TapRouletteHomeScreenState extends State<TapRouletteHomeScreen> {
                           const RandomNumberDisplay(),
                         ],
                       ),
-                      // Text(
-                      //   '$_counter 90',
-                      //   style: Theme.of(context).textTheme.headlineMedium,
-                      // ),
                     ],
                   ),
                   ConfettiWidget(
@@ -119,11 +111,6 @@ class _TapRouletteHomeScreenState extends State<TapRouletteHomeScreen> {
           );
         }),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
